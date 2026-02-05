18e059206070d98fb2094e46fae8d991285e07ea
thanhdattinhdat1411-sketch <thanhdattinhdat1411@gmail.com>
2026-02-05 11:09:47 +0700

    Nguyễn Quang Thành Đạt nộp lap 6

--- bai1.c
+++ bai1.c
@@ -11,3 +11,40 @@
 
 // VIẾT CODE Ở ĐÂY
 
+#include <stdio.h>
+
+int main() {
+    int n;
+    int arr[100];
+    int tong = 0;
+    int dem = 0;
+    float trungBinh;
+
+    // Nhập số phần tử của mảng
+    printf("Nhap so phan tu cua mang: ");
+    scanf("%d", &n);
+
+    // Nhập các phần tử của mảng
+    for (int i = 0; i < n; i++) {
+        printf("Nhap arr[%d]: ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    // Tính tổng các số chia hết cho 3
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 3 == 0) {
+            tong += arr[i];
+            dem++;
+        }
+    }
+
+    // Tính và xuất trung bình
+    if (dem > 0) {
+        trungBinh = (float)tong / dem;
+        printf("Trung binh cong cac so chia het cho 3 la: %.2f", trungBinh);
+    } else {
+        printf("Khong co so nao chia het cho 3 trong mang");
+    }
+
+    return 0;
+}
